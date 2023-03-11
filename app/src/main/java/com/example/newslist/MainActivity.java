ef50f342caa4e9f55eec69e071ec23712078b0d2
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2023-03-11 19:58:04 +0700

    add webview

--- app/src/main/java/com/example/newslist/MainActivity.java
+++ app/src/main/java/com/example/newslist/MainActivity.java
@@ -2,26 +2,35 @@ package com.example.newslist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView mWebview;
+
     ListView list;
 
-    String[] maintitle ={
-            "Github","VnExpress",
-            "Vietnamnet","Bao moi",
+    String[] maintitle = {
+            "Github", "VnExpress",
+            "Vietnamnet", "Bao moi",
             "Bao Tuoi tre",
     };
 
-    String[] subtitle ={
-            "My github page","VnExpress Page",
-            "Vietnamnet Page","Bao moi Page",
+    String[] subtitle = {
+            "My github page", "VnExpress Page",
+            "Vietnamnet Page", "Bao moi Page",
             "Bao tuoi tre Page",
     };
     String[] urls = {
@@ -32,29 +41,65 @@ public class MainActivity extends AppCompatActivity {
             "https://tuoitre.vn/",
     };
 
-    Integer[] imgid={
-            R.drawable.news,R.drawable.news,
-            R.drawable.news,R.drawable.news,
+    Integer[] imgid = {
+            R.drawable.news, R.drawable.news,
+            R.drawable.news, R.drawable.news,
             R.drawable.news,
     };
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.mWebview.canGoBack()) {
+            this.mWebview.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle,imgid,urls);
-        list=(ListView)findViewById(R.id.list);
+        MyListAdapter adapter = new MyListAdapter(this, maintitle, subtitle, imgid, urls);
+        list = (ListView) findViewById(R.id.list);
         list.setAdapter(adapter);
+        mWebview = new WebView(this);
+
+        mWebview.getSettings().setJavaScriptEnabled(true);
+        mWebview.getSettings().setSupportZoom(true);
+        mWebview.getSettings().setAllowContentAccess(true);
+        mWebview.getSettings().setBuiltInZoomControls(true);
+        mWebview.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        mWebview.getSettings().setDisplayZoomControls(false);
+        final Activity activity = this;
+        mWebview.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+
+        });
 
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
-            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // TODO Auto-generated method stub
-                    //Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
-                    Intent openLinksIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(urls[position]));
-                startActivity(openLinksIntent);
+                //Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
+
+                mWebview.loadUrl(urls[position]);
+                setContentView(mWebview);
             }
         });
     }
--- app/src/main/java/com/example/newslist/MyListAdapter.java
+++ app/src/main/java/com/example/newslist/MyListAdapter.java
@@ -19,20 +19,20 @@ public class MyListAdapter extends ArrayAdapter<String> {
     private final String[] urls;
 
 
-    public MyListAdapter(Activity context, String[] maintitle,String[] subtitle, Integer[] imgid, String[] urls) {
+    public MyListAdapter(Activity context, String[] maintitle, String[] subtitle, Integer[] imgid, String[] urls) {
         super(context, R.layout.mylist, maintitle);
         // TODO Auto-generated constructor stub
 
-        this.context=context;
-        this.maintitle=maintitle;
-        this.subtitle=subtitle;
-        this.imgid=imgid;
+        this.context = context;
+        this.maintitle = maintitle;
+        this.subtitle = subtitle;
+        this.imgid = imgid;
         this.urls = urls;
     }
 
-    public View getView(int position,View view,ViewGroup parent) {
-        LayoutInflater inflater=context.getLayoutInflater();
-        View rowView=inflater.inflate(R.layout.mylist, null,true);
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.mylist, null, true);
 
         TextView titleText = (TextView) rowView.findViewById(R.id.title);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
@@ -44,5 +44,5 @@ public class MyListAdapter extends ArrayAdapter<String> {
 
         return rowView;
 
-    };
+    }
 }
